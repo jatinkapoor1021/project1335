047ca5f04a68673f41082f729b18e9121cb6f59e
jatinkapoor1021 <31459506+jatinkapoor1021@users.noreply.github.com>
2024-03-01 18:09:03 -0500

    Update Book.cpp

--- Book.cpp
+++ Book.cpp
@@ -1,32 +1,213 @@
 #include "Book.hpp"
-#include <iostream>
-#include <iomanip>
 
-// Constructor
-Book::Book(const std::string& title, const std::string& author, long long int ISBN,
-           int* icon, float price, const std::vector<std::string>& keywords, const std::string& blurb)
-    : title_(title), author_(author), ISBN_(ISBN), icon_(icon), price_(price), keywords_(keywords), blurb_(blurb) {}
+// write and document all methods in this file.
 
-// Destructor
-Book::~Book() {
-    delete[] icon_;
+// default constructor: Book::Book
+Book::Book()
+{
+    title_ = "";
+    author_ = "";
+    ISBN_ = 0;
+    icon_ = nullptr;
+    price_ = 0.0;
+    keywords_ = {};
+    blurb_ = "";
 }
 
-// Print method
-void Book::print() const {
-    std::cout << "Title: " << title_ << std::endl;
-    std::cout << "Author: " << author_ << std::endl;
-    std::cout << "ISBN: " << ISBN_ << std::endl;
-    std::cout << "Icon: ";
-    for (int i = 0; i < 80; ++i) {
-        std::cout << icon_[i] << " ";
+// destructor Book::~Book()
+Book::~Book()
+{
+    delete[] icon_;
+    icon_ = nullptr;
+}
+
+// copy constructor Book::Book(const Book &rhs)
+Book::Book(const Book &rhs)
+{
+    title_ = rhs.title_;
+    author_ = rhs.author_;
+    ISBN_ = rhs.ISBN_;
+
+    if (rhs.icon_ != nullptr)
+    {
+        icon_ = new int[80];
+        std::copy(rhs.icon_, rhs.icon_ + 80, icon_);
     }
-    std::cout << std::endl;
-    std::cout << "Price: $" << std::fixed << std::setprecision(2) << price_ << std::endl;
-    std::cout << "Keywords: ";
-    for (const auto& keyword : keywords_) {
-        std::cout << keyword << " ";
+    else
+        icon_ = nullptr;
+
+    price_ = rhs.price_;
+    keywords_ = rhs.keywords_;
+    blurb_ = rhs.blurb_;
+}
+
+// copy assignment Book &Book::operator=(const Book &rhs)
+Book &Book::operator=(const Book &rhs)
+{
+    Book temp = rhs;
+    std::swap(*this, temp);
+    return *this;
+}
+
+// moving the constructor Book::Book(Book &&rhs)
+Book::Book(Book &&rhs)
+{
+    title_ = std::move(rhs.title_);
+    author_ = std::move(rhs.author_);
+    ISBN_ = rhs.ISBN_;
+
+    icon_ = rhs.icon_;
+    rhs.icon_ = nullptr;
+
+    price_ = rhs.price_;
+
+    keywords_ = std::move(rhs.keywords_);
+    blurb_ = std::move(rhs.blurb_);
+}
+
+// moving assignment operator Book &Book::operator=(Book &&rhs)
+Book &Book::operator=(Book &&rhs)
+{
+    if (this != &rhs)
+    {
+        title_ = std::move(rhs.title_);
+        author_ = std::move(rhs.author_);
+        ISBN_ = rhs.ISBN_;
+
+        icon_ = rhs.icon_;
+        rhs.icon_ = nullptr;
+
+        price_ = rhs.price_;
+
+        keywords_ = std::move(rhs.keywords_);
+        blurb_ = std::move(rhs.blurb_);
     }
-    std::cout << std::endl;
-    std::cout << "Blurb: " << blurb_ << std::endl;
+    return *this;
+}
+
+
+//Book::getTitle()
+const std::string &Book::getTitle() const
+{
+    return title_;
+}
+
+//void Book
+void Book::setTitle(const std::string &title)
+{
+    title_ = title;
+}
+
+
+//getAuthor
+const std::string &Book::getAuthor() const
+{
+    return author_;
+}
+
+
+//setAuthor
+void Book::setAuthor(const std::string &author)
+{
+    author_ = author;
+}
+
+//int Book::getISBN()
+long long int Book::getISBN() const
+{
+    return ISBN_;
+}
+
+//Book::setISBN
+void Book::setISBN(long long int ISBN)
+{
+    ISBN_ = ISBN;
+}
+//getIcon
+const int *Book::getIcon() const
+{
+    return icon_;
+}
+//setIcon
+void Book::setIcon(int *icon)
+{
+    icon_ = icon;
+}
+
+//getPrice
+float Book::getPrice() const
+{
+    return price_;
+}
+
+//setPrice
+void Book::setPrice(float price)
+{
+    price_ = price;
+}
+
+//getKeywords
+const std::vector<std::string> &Book::getKeywords() const
+{
+    return keywords_;
+}
+
+
+//setKeywords
+void Book::setKeywords(const std::vector<std::string> &keywords)
+{
+    keywords_ = keywords;
+}
+
+
+//getBlurb
+const std::string &Book::getBlurb() const
+{
+    return blurb_;
+}
+
+//setBlurb
+void Book::setBlurb(const std::string &blurb)
+{
+    blurb_ = blurb;
+}
+
+
+
+// constructor  void Book::print()
+void Book::print() const
+{
+    std::string keywords;
+    for (auto kw = this->getKeywords().begin(); kw != this->getKeywords().end(); kw++)
+    {
+        if (kw == this->getKeywords().end() - 1)
+        {
+            keywords += (*kw);
+        }
+        else
+        {
+            keywords += (*kw + ", ");
+        }
+    }
+
+    std::string icon;
+    for (int i = 0; i < 80; i++)
+    {
+        if (i == 79)
+        {
+            icon += std::to_string(icon_[i]);
+        }
+        else
+        {
+            icon += std::to_string(icon_[i]) + " ";
+        }
+    }
+
+    std::cout << "Title: " << title_ << "\n"
+              << "Author: " << author_ << "\n"
+              << "ISBN: " << ISBN_ << "\n"
+              << "Icon: " << icon << "\n"
+              << "Price: $" << std::fixed << std::setprecision(2) << price_ << "\n"
+              << "Keywords: " << keywords << "\n"
+              << "Blurb: " << blurb_ << "\n";
 }
